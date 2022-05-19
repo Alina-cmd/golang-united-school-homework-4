e475d6f0a14df9b0f4f031fc8262c149ff37ebb3
Alina <you@example.com>
2022-05-19 16:58:11 +0300

    hw_4

--- string_sum.go
+++ string_sum.go
@@ -2,6 +2,9 @@ package string_sum
 
 import (
 	"errors"
+	"fmt"
+	"strconv"
+	"strings"
 )
 
 //use these errors as appropriate, wrapping them with fmt.Errorf function
@@ -23,5 +26,52 @@ var (
 // Use the errors defined above as described, again wrapping into fmt.Errorf
 
 func StringSum(input string) (output string, err error) {
-	return "", nil
+
+	// delete whitespaces
+	input = strings.ReplaceAll(input, " ", "")
+
+	// is empty
+	if input == "" {
+		return "", fmt.Errorf(" Empty Input in StringSum(): %w", errorEmptyInput)
+	}
+
+	// contain extra symbols
+	symbols := []rune{45, 43, 49, 50, 51, 52, 53, 54, 55, 56, 57, 32}
+	inputRunes := []rune(input)
+	for _, v := range inputRunes {
+		if find(v, symbols) == -1 {
+			_, errAtoi := strconv.Atoi(string(v))
+			return "", fmt.Errorf("Contain extra symbol: %w", errAtoi)
+		}
+	}
+
+	// count of numbers
+	amount := 0
+	input = strings.ReplaceAll(input, "+", "/+")
+	input = strings.ReplaceAll(input, "-", "/-")
+	count := strings.Split(input, "/")
+	start := 0
+	if count[0] == "" {
+		amount--
+		start = 1
+	}
+	amount += len(count)
+	if amount != 2 {
+		return "", fmt.Errorf(" Error in StringSum: %w", errorNotTwoOperands)
+	}
+	first, _ := strconv.Atoi(count[start])
+	second, _ := strconv.Atoi(count[start+1])
+	output = strconv.Itoa(first + second)
+
+	return output, nil
+}
+
+func find(what rune, where []rune) (idx int) {
+	idx = 1
+	for _, v := range where {
+		if v == what {
+			return
+		}
+	}
+	return -1
 }
